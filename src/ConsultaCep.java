ca79eb88aeb7172250ea587e7f347593d1bcbb40
v1nizx <144844828+v1nizx@users.noreply.github.com>
2024-05-23 11:00:25 -0300

    Add files via upload

--- src/ConsultaCep.java
+++ src/ConsultaCep.java
@@ -10,20 +10,17 @@ import java.net.http.HttpResponse;
 public class ConsultaCep {
     public Endereco buscaEndereco(String cep) {
         URI endereco = URI.create("https://viacep.com.br/ws/" + cep + "/json/");
-
-        HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(endereco)
                 .build();
-        HttpResponse<String> response = null;
         try {
-            response = HttpClient
+            HttpResponse<String> response = HttpClient
                     .newHttpClient()
                     .send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException  |InterruptedException e){
+            return new Gson().fromJson(response.body(), Endereco.class);
+        } catch (Exception e){
             throw new RuntimeException("Nao consegui obter o endereco a partir desse CEP!");
         }
 
-        return new Gson().fromJson(response.body(), Endereco.class);
 }
 }
--- src/PrincipalBusca.java
+++ src/PrincipalBusca.java
@@ -1,8 +1,32 @@
+import java.io.IOException;
+import java.util.Scanner;
 
 public class PrincipalBusca {
     public static void main(String[] args) {
-     ConsultaCep consultacep = new ConsultaCep();
-     Endereco novoEndereco = consultacep.buscaEndereco("65051600");
-        System.out.println(novoEndereco);
+        Scanner ler = new Scanner(System.in);
+        String busca = "";
+        ConsultaCep consultacep = new ConsultaCep();
+
+        while (!busca.equalsIgnoreCase("sair")) {
+            System.out.println("Digite um cep para leitura: ");
+            busca = ler.nextLine();
+            if (busca.equalsIgnoreCase("0")){
+                break;
+            }else if (busca.equalsIgnoreCase("1")){
+                continue;
+            }
+            try {
+                Endereco novoEndereco = consultacep.buscaEndereco(busca);
+                System.out.println(novoEndereco);
+                GeradorArquivo gerador = new GeradorArquivo();
+                gerador.salvaJson(novoEndereco);
+                System.out.println("Arquivo Salvo! ");
+            } catch (RuntimeException | IOException e) {
+                System.out.println(e.getMessage());
+                System.out.println("Finalizando a aplicacao! ");
+            }
+            System.out.println("Aplicação finalizada! ");
+            System.out.println("Digite 0 para sair ou 1 para continuar: ");
+        }
     }
 }
